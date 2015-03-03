cfd71c3885ede6ad0bc11c5956c9c15cb8e72f76
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-03-03 09:57:30 +0100

    Implement the main functionality

--- main.go
+++ main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+
+	"github.com/peterh/liner"
 )
 
 var (
-	ivy = flag.String("ivy", "ivy", "the path to ivy")
+	path = flag.String("ivy", "ivy", "the path to ivy")
 )
 
 func main() {
 	flag.Usage = usage
 	flag.Parse()
+
+	reader, writer := io.Pipe()
+
+	liner := liner.NewLiner()
+	liner.SetCtrlCAborts(true)
+	defer liner.Close()
+
+	cmd := exec.Command(*path)
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = reader, os.Stdout, os.Stderr
+	cmd.Start()
+
+	for {
+		if line, err := liner.Prompt(""); err == nil {
+			liner.AppendHistory(line)
+			fmt.Fprintln(writer, line)
+		} else {
+			break
+		}
+	}
 }
 
 func usage() {
